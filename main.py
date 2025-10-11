db3ece883278e8441effcb28d0ebba3855912204
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-10-11 13:15:07 +0300

    lesson3

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from direct.showbase.ShowBase import ShowBase
+
+from hero import Hero
 from mapmanager import MapManager
+
+
 # Клас гри
 class Game(ShowBase):
     def __init__(self):
@@ -7,6 +11,7 @@ class Game(ShowBase):
 
         self.land = MapManager(self)
         self.land.loadMap("map/map_position.txt", "map/map_textures.txt")
+        self.hero = Hero(self, (5, 5, 2), self.land)
         self.camLens.setFov(90)
 
 
